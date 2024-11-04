6045cc06d8343e9771b6eb1e742974e21cb37a76
pranay patil <pranay7505@gmail.com>
2024-11-04 08:55:00 +0530

    creating and update frontend

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7072/api/GetResumeCounter';
+const functionApiUrl = "https://getresumecounter7505.azurewebsites.net/api/GetResumeCounter?code=jLHDrE7V0mEDYlOYzbF74xeP0edBN4L2Swuqc29M19eIAzFuiEY7BA%3D%3D";
+const localFunctionApi = 'http://localhost:7072/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
